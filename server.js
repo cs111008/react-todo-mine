0415f23230657ba30f794b719515610b29b35720
Aman Soni <ASoni@professionalaccess.com>
2017-06-23 15:42:12 +0530

    fix localhost

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const PORT = process.env.PORT || 3000;
 
 var app = express();
 
+app.use(function(req,res,next){
+	if(req.headers['x-forwarded-proto']==='https'){
+		res.redirect('http://' + req.hostname + req.url);
+	} else {
+		
+		next()
+	}
+})
+
 app.use(express.static('public'));
 
 app.listen(PORT, () => {
